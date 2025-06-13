f34f92f5c42a15284c8e4d3c8d4c2bd77f1d0737
AnandVivek-iiti <anand1234.vivek@gmail.com>
2025-06-13 10:02:23 +0530

    Home page completed

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import react from '@vitejs/plugin-react';
-
+import netlify from '@netlify/vite-plugin'
 export default defineConfig({
-  plugins: [tailwindcss(),react(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(),react(), reactRouter(), tsconfigPaths() ,netlify()],
 });
